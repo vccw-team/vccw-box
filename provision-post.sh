ebe5edd2bb87b6d2f2c840e0c01b0ae1120c926c
miya0001 <miya@wpist.me>
2015-05-13 04:02:39 +0900

    some fix

---
+++ provision-post.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -ex
+
+yum -y distro-sync
+yum -y clean all
+
+/etc/init.d/httpd stop
+
+rm -fr /var/www/wordpress
+rm -f /etc/httpd/sites-available/wordpress.conf
+rm -f /etc/httpd/sites-enabled/wordpress.conf
+
+rm -f /etc/ssh/ssh_host_*
+cd /var/log
+find /var/log/ -type f -name '*.log' -exec cp /dev/null {} \;
+cp /dev/null /var/log/syslog
+
+yes | cp /dev/null /root/.bash_history
+yes | cp /dev/null /home/vagrant/.bash_history
+
+/etc/init.d/vboxadd setup
+
+ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
+
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
+
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 755 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/authorized_keys
+
+history -c
+
+dd if=/dev/zero of=empty bs=1M
+rm empty
+
+shutdown -r now
